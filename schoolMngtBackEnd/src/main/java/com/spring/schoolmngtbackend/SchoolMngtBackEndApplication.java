e71c462716aeb4806be81749cccead09df370731
Abdoulaye Aliou SALL <87236495+babagl@users.noreply.github.com>
2022-10-06 12:52:46 +0100

    Add files via upload

---
+++ schoolMngtBackEnd/src/main/java/com/spring/schoolmngtbackend/SchoolMngtBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.spring.schoolmngtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMngtBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMngtBackEndApplication.class, args);
+    }
+
+}
---
+++ schoolMngtBackEnd/src/test/java/com/spring/schoolmngtbackend/SchoolMngtBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.schoolmngtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMngtBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
